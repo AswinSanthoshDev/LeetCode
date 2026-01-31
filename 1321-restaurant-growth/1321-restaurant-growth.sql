32b6184dceeb240815cec592b19cc67a001331fe
Aswin Santhosh <aswinsanthosh2004.dev@gmail.com>
2026-01-31 19:34:44 +0530

    Time: 237 ms (9.45%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,30 @@
+-- Write your PostgreSQL query statement below
+select
+    visited_on,
+    amount,
+    average_amount
+from (
+    select
+        visited_on,
+        sum(amount) over (
+            order by visited_on
+            rows between 6 preceding and current row
+        ) as amount,
+        round(
+            avg(amount) over (
+                order by visited_on
+                rows between 6 preceding and current row
+            ),
+            2
+        ) as average_amount,
+        row_number() over (order by visited_on) as rn
+    from (
+        select
+            visited_on,
+            sum(amount) as amount
+        from customer
+        group by visited_on
+    ) d
+) x
+where rn >= 7
+order by visited_on;
